fd8484713c321954993bae0db04c377b95979be8
UmmuIzzatulWidyatama <ummu.izzatul@widyatama.ac.id>
2025-07-14 00:10:29 +0700

    homework sesi 13

---
+++ test/appdemo.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+//Nama test suite
+describe('API Demo testing', () => {
+
+  //nama test case
+  it('Coba test buka aplikasinya', async() => {
+    console.log('Aplikasi terbuka!');
+  });
+  
+})
